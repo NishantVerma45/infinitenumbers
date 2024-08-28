9a5fbd9aa8cb7cce95381180e56a37c378798039
NishantVerma45 <100504337+NishantVerma45@users.noreply.github.com>
2024-08-28 17:36:16 +0530

    Update format_funcn.py

--- format_funcn.py
+++ format_funcn.py
@@ -7,6 +7,7 @@ for i in range(0,len(a)):
     template = "{} is in {}"
     print(template.format(a[i],b[i]))
 '''
+'''
 str1 = "my name is {nm}, I am {age}"
 print(str1.format(nm = "Nishant" , age = "18"))
 '''
